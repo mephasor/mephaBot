06dc58760b6b46629e40e4eeff38a100d4d070c5
mephasor <kons.na@gmail.com>
2016-03-31 19:10:43 -0400

    changed the code to use the radio list.

--- runBot.py
+++ runBot.py
@@ -12,138 +12,116 @@ if not discord.opus.is_loaded():
     discord.opus.load_opus('/usr/local/lib/libopus.so')
 
 
-client = discord.Client()
 
 
-class MephaBot:
+class MephaBot(discord.Client):
 
     # Class Variables:
 
     # Config Reader Result:
     cfg = ''
+    commands = {}
 
-    # Discord Client
-    client = ''
+    # Radio station dictionary
+    radio = {}
 
-    # Audio Player and its status
+    # Audio Player status
     player = 0
+
+    defChannelName = '♫ Муз. Студия ♫'
 
     # TODO: change this to an enum
     # 0 stopped, 1 playing, 2 paused
     playerStatus = 0
 
-    # command list
-    commands = {
-        '!shrug': botShrug,
-        '!exit': botExit,
-        '!bot': botJoinVoiceChannel,
-        '!piterfm': botPlayPiterFM,
-        '!nashe': botPlayNashe,
-        '!1live': botPlay1Live,
-        '!stop': botStop
-    }
-
     # Constructor
-    def __init__(self, cfg, client):
+    def __init__(self, cfg):
+        super().__init__()
         self.cfg = cfg
-        self.client = client
+
+        configFile = open('cfg/radio.cfg').readlines()
+        for line in configFile:
+            tmp = line.split(', ')
+            self.radio[tmp[0]] = tmp[1].rstrip('\n')
+
 
     def runBot(self):
-        client.run(self.cfg.getUsername(), self.cfg.getPW())
+       super().run(self.cfg.getUsername(), self.cfg.getPW())
 
     #Command Execution functions
-    async def botShrug(message):
-        await client.send_message(message.channel,
+    async def botShrug(self, message):
+        await self.send_message(message.channel,
                                   message.author.mention + u": ¯\_(ツ)_/¯")
 
-    async def botExit(message):
-        await client.send_message(message.channel, "Bot exiting now. bye bye :( ")
-        client.logout()
+    async def botExit(self, message):
+        await self.send_message(message.channel, "Bot exiting now. bye bye :( ")
+        self.logout()
         exit()
 
-    async def botJoinVoiceChannel(message):
-        if client.is_voice_connected():
-            await client.send_message(message.channel,
+    async def botJoinVoiceChannel(self, message):
+        if self.is_voice_connected():
+            await self.send_message(message.channel,
                                       'Already connected to a voice channel')
         channel_name = '♫ Муз. Студия ♫'
         print('Trying to join: %s' % (channel_name))
         check = lambda c: c.name == channel_name and c.type == discord.ChannelType.voice
         channel = discord.utils.find(check, message.server.channels)
         if channel is None:
-            await client.send_message(message.channel,
+            await self.send_message(message.channel,
                                       'Cannot find a voice channel by that name.')
         else:
-            await client.join_voice_channel(channel)
-            client.starter = message.author
-
-    async def botPlayPiterFM(message):
-        global player
-        global playerStatus
-
-        if(playerStatus is not 0):
-            player.stop()
-
-        player = client.voice.create_ffmpeg_player(
-            'http://icecast.piktv.cdnvideo.ru/piterfm?81')
-        player.start()
-        playerStatus = 1
-
-    async def botPlayNashe(message):
-        global player
-        global layerStatus
-
-        if(playerStatus is not 0):
-            player.stop()
-
-        player = client.voice.create_ffmpeg_player(
-            'http://nashe1.hostingradio.ru:80/nashe-128.mp3')
-        player.start()
-        playerStatus = 1
-
-    async def botPlay1Live(message):
-        global player
-        global playerStatus
-
-        if(playerStatus is not 0):
-            player.stop()
-
-        player = client.voice.create_ffmpeg_player(
-            'http://1live.akacast.akamaistream.net/7/706/119434/v1/gnl.akacast.akamaistream.net/1live')
-        #piterPlayer = client.voice.create_stream_player('http://icecast.piktv.cdnvideo.ru/piterfm')
-        player.start()
-        playerStatus = 1
+            await self.join_voice_channel(channel)
+            self.starter = message.author
 
 
-    async def botStop(message):
-        player.stop()
-        playerStatus = 0
-        print(player.is_done())
+    async def botStop(self, message):
+        self.player.stop()
+    async def botPlayRadio(self, message):
+        if(self.playerStatus is not 0):
+            self.player.stop()
 
+        station = message.content[1:]
+        radioUrl = self.radio[station]
+        print('Starting to play Radio Station: '+station)
+        self.player = self.voice.create_ffmpeg_player(radioUrl)
+        self.player.start()
+        self.playerStatus = 1
+
+    # command list
+    commands = {
+        '!shrug': botShrug,
+        '!exit': botExit,
+        '!bot': botJoinVoiceChannel,
+        '!piterfm': botPlayRadio,
+        '!nashe': botPlayRadio,
+        '!1live': botPlayRadio,
+        '!stop': botStop
+    }
 
 
 # Event Handlers
-    @client.event
-    async def on_ready():
+    async def on_ready(self):
         print('Logged in as')
-        print(client.user.name)
-        print(client.user.id)
+        print(self.user.name)
+        print(self.user.id)
         print('------')
 
 
-    @client.event
-    async def on_message(message):
+    async def on_message(self, message):
 
-        for key in commands:
+
+        for key in self.commands:
             if(message.content.startswith(key)):
-                rtrn = await commands[key](message)
-                await client.delete_message(message)
+                print(self.user.name)
+                rtrn = await self.commands[key](self, message)
+                await self.delete_message(message)
 
 
 def main():
     cfg = configReader.ConfigReader()
     cfg.readConfig()
-    myBot = MephaBot(cfg, client)
-
+    myBot = MephaBot(cfg)
+    myBot.runBot()
 
 if __name__ == "__main__":
     main()
